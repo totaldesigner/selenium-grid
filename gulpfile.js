0ed305cbc25275d88accb4fce1aa1952f3d7dfdd
mspark <mspark@humaxdigital.com>
2016-12-05 11:04:04 +0900

    fire commit

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+/*jshint node: true, camelcase: false*/
+/*global require: true*/
+
+'use strict';
+
+var gulp = require('gulp'),
+    gulpProtractorAngular = require('gulp-angular-protractor');
+
+// Setting up the test task
+gulp.task('e2e', function (callback) {
+    gulp
+        .src(['example_spec.js'])
+        .pipe(gulpProtractorAngular({
+            'configFile': 'protractor.conf.js',
+            'debug': false,
+            'autoStartStopServer': true,
+            'verbose': false,
+            'webDriverUpdate': {
+                'browsers': ['ie'],
+                'args': ['--ie32']
+            },
+            'webDriverStart': {
+                'args': ['--ie32']
+            }
+        }))
+        .on('error', function (e) {
+            console.log(e);
+        })
+        .on('end', callback);
+});
